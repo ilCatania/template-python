41a540f125b3888a9e50536a624731d599b8bf02
Gabriele Catania <gabriele.ctn@gmail.com>
2023-03-16 21:41:12 +0000

    minor api cleanup

--- {{cookiecutter.repo_name}}/tests/conftest.py
+++ {{cookiecutter.repo_name}}/tests/conftest.py
@@ -8,7 +8,7 @@ import pytest
 @pytest.fixture
 def testfile() -> Callable[[Union[str, os.PathLike]], Path]:
     """Shortcut to obtain paths to files under `tests/__files`."""
-    test_files_dir = (Path(__file__).parent / "__files").resolve()
+    test_files_dir = Path(__file__).parent.joinpath("__files").resolve()
     assert test_files_dir.is_dir(), f"{test_files_dir} is not a directory!"
 
     def testfile_fn(p: Union[str, os.PathLike]) -> Path:
